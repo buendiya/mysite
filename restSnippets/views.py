e55714e12d94eb8767937a36d9ca8ee2af8ee6fb
buendiya <jingshuaizhen@gmail.com>
2014-07-31 10:42:07 +0800

    finish REST framework tutorial 4

--- restSnippets/views.py
+++ restSnippets/views.py
@@ -1,15 +1,24 @@
 from restSnippets.models import Snippet
 from restSnippets.serializers import SnippetSerializer, UserSerializer
 from rest_framework import generics
+from rest_framework import permissions
+from restSnippets.permissions import IsOwnerOrReadOnly
 
 class SnippetList(generics.ListCreateAPIView):
     queryset = Snippet.objects.all()
     serializer_class = SnippetSerializer
-
+    permission_classes = (permissions.IsAuthenticatedOrReadOnly,)
+    
+    def pre_save(self, obj):
+        obj.owner = self.request.user
 
 class SnippetDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = Snippet.objects.all()
     serializer_class = SnippetSerializer
+    permission_classes = (permissions.IsAuthenticatedOrReadOnly,)
+    permission_classes = (permissions.IsAuthenticatedOrReadOnly, IsOwnerOrReadOnly,)
+    def pre_save(self, obj):
+        obj.owner = self.request.user
 
 from django.contrib.auth.models import User
 
